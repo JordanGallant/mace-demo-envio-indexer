1206083ec53da4aafaa4e61359e56abff670f096
JordanGallant <39-12602@sae.edu.za>
2025-03-06 11:37:07 +0100

    mace stats

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -6,7 +6,31 @@ import {
   IDexRouter_SwapCompleted,
 } from "generated";
 
+
+const createOrUpdateGlobalStats = async (fields: any, context: any) => {
+  let globalStats = await context.GlobalStats.get("global");
+
+  if (!globalStats) {
+    globalStats = {
+      id: "global",
+      inputToken: "", // assumed to be 1 if not set, should have an event emitted on initialization
+    };
+  }
+
+  // Update only the fields that exist in the event object
+  for (const key in fields) {
+    if (fields[key] !== undefined && key in globalStats) {
+      globalStats[key] = fields[key];
+    }
+  }
+
+  await context.GlobalStats.set(globalStats);
+};
+
 IDexRouter.SwapCompleted.handler(async ({ event, context }) => {
+
+  let {inputToken, amountIn, outputToken, amountOut} = event.params;
+
   const entity: IDexRouter_SwapCompleted = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     inputToken: event.params.inputToken,
@@ -15,8 +39,45 @@ IDexRouter.SwapCompleted.handler(async ({ event, context }) => {
     amountOutMin: event.params.amountOutMin,
     amountOut: event.params.amountOut,
   };
+  let tokenIn = await context.Token.get(inputToken);
+  if (tokenIn) {
+    tokenIn = {
+      ...tokenIn,
+      tradeInAmount: tokenIn.tradeInAmount + amountIn,
+      totalAmount: tokenIn.totalAmount + amountIn,
+    };
+  } else {
+    tokenIn = {
+      id: inputToken,
+      tradeInAmount: amountIn,
+      tradeOutAmount: BigInt(0),
+      totalAmount: amountIn,
+    };
+  }
+
+  let tokenOut = await context.Token.get(outputToken);
+  if (tokenOut) {
+    tokenOut = {
+      ...tokenOut,
+      tradeOutAmount: tokenOut.tradeOutAmount + amountOut,
+      totalAmount: tokenOut.totalAmount + amountOut,
+    };
+  } else {
+    tokenOut = {
+      id: outputToken,
+      tradeInAmount: BigInt(0),
+      tradeOutAmount: amountOut,
+      totalAmount: amountOut,
+    };
+  }
+
+    context.Token.set(tokenIn);
+    context.Token.set(tokenOut);
+
 
   context.IDexRouter_SwapCompleted.set(entity);
+  await createOrUpdateGlobalStats({inputToken: event.params.inputToken}, context);
+
 });
 
 //Swaps
